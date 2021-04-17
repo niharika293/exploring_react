b8502456cdae4eaf09288a9b984231f921f218cb
Niharika Gurnani <niharikagurnani13@gmail.com>
2021-04-17 18:12:45 +0530

    Fetch data from posts API

--- src/Components/Post.js
+++ src/Components/Post.js
@@ -2,6 +2,7 @@
 // () components are light, faster because of less  dependencies.
 // these are stateless, although states can be over-ridden using React hooks.
 import React from 'react'
+// import PostsList from './PostsList'
 
 // props : these are like () attributes in JS & attributes in HTML.
 // each post will be recieveing some data through props.
@@ -11,7 +12,8 @@ function Post(props) {
     }
   return (
     <li className = "PostItem" style= {postStyle}>
-
+        {props.post.content}
+        {props.post.user.name}
     </li>
   );
 }
--- src/Components/PostsList.js
+++ src/Components/PostsList.js
@@ -2,20 +2,56 @@
 // Class - Based Component
 
 import React from 'react'
+import axios from 'axios'
+import Post from './Post';
+// To connect to API using PostList component use Axios library.
 
-class PostsList extends React.Component() {
+
+class PostsList extends React.Component {
+
+    // similar to the () based components recieving props in args.
+    constructor(props){
+        super(props); //adapts from parent class props.
+        // React works on states. A component has a state.
+        // A state is basically a set of all those variables which can change further & further.
+
+        this.state = {
+            posts : [] //initially empty, but will change with time.
+        }
+
+        this.getPosts(); //whenever the instance of this class is created, this () is called.
+    }
+    getPosts(){
+        // will use axios library to fetch the posts.
+
+        axios.get('http://localhost:8000/api/v1/posts').then((data) => {
+            this.setState({posts : data.data.posts})});
+        }
+    // () to apply styles to the post
+
     // instead of returning directly, use render()
     render(){
+        let postStyle = {
+            color : '#e91e63'
+        }
         return (
             <div>
-                <h1>Codeial - Posts</h1>
+                <h1 style = {postStyle}>Codeial - Posts</h1>
                 {/* This ul will itearte through the posts API & fetch posts & return them using the posts component. */}
+                {/* this.state.posts.map((post) (A) =>
+                    (B) <Post post = {post}  
+                    (C)key = {post._id}></Post>) */}
                 <ul>
-                    
+                    {
+                        this.state.posts.map((post) => <Post post = {post} key = {post._id}></Post>)
+                    }
+                    {/* (A) map will loop around each post in the array. */}
+                    {/* (B) Imported the component post & set-up it's props(post) */}
+                    {/* (C) setting up a key as post-id for this post component. */}
                 </ul>
             </div>
           );
     }
 
 }
-export default Post;
+export default PostsList;
